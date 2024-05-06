4f87817c76052ac33901f6e3d4b128a7854953ae
Malakabouelenein <158231831+Malakabouelenein@users.noreply.github.com>
2024-05-06 21:17:57 +0300

    Level 1st version completion + build

--- Assets/Scripts/characterMove.cs
+++ Assets/Scripts/characterMove.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+//using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class characterMove : MonoBehaviour
 {
